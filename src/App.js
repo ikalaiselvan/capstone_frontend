e102c39eebb39442b5760e76b23c2ae8b84f7804
Kalaiselvan <ikalaiselvan@users.noreply.github.com>
2023-01-01 06:04:34 +0530

    Local data deleted

--- src/App.js
+++ src/App.js
@@ -9,29 +9,6 @@ import Footer from "./Footer/Footer";
 
 function App() {
   let [apiData, setApiData] = useState({});
-  
-  // let apiData = {
-  //   ask: 150017.7,
-  //   bid: 150007.7,
-  //   ch: -290.8,
-  //   chp: -0.19,
-  //   currency: "INR",
-  //   exchange: "IDC",
-  //   high_price: 150168.9,
-  //   low_price: 149918.1,
-  //   metal: "XAU",
-  //   open_price: 150087,
-  //   open_time: 1672185600,
-  //   prev_close_price: 150298.5,
-  //   price: 150007.7,
-  //   price_gram_18k: 3617.1447,
-  //   price_gram_20k: 4019.0496,
-  //   price_gram_21k: 4220.0021,
-  //   price_gram_22k: 4420.9546,
-  //   price_gram_24k: 4822.8595,
-  //   symbol: "FX_IDC:XAUINR",
-  //   timestamp: 1672200530,
-  // };
 
   var myHeaders = new Headers();
   myHeaders.append("x-access-token", "goldapi-ps6l1uatlc74dxgu-io");
